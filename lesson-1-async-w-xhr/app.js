e5807b2553e25199eeeb698531b2a6f2cedf2bf8
Jackie Wellons <jmw5050@gmail.com>
2018-07-02 15:17:16 -0700

    feat- add img works with search to api unsplash

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,31 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+    
+    const imgRequest = new XMLHttpRequest();
+    imgRequest.onload = addImage;
+    imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+    imgRequest.setRequestHeader('Authorization', 'Client-ID c317071980658d0800e2ba3f633bd5bcdf3a6d9bab61527ce3cd65eb723015a3');
+    imgRequest.send();
+
+    const artRequest = new XMLHttpRequest();
     });
+
+
+  function addImage() {
+    let htmlContent;
+    const data = JSON.parse(this.responseText);
+    console.log(data.results[0].urls.small);
+    if(data && data.results && data.results[0]) {
+      const firstImg = data.results[0];
+      htmlContent = `<figure>
+        <img src="${firstImg.urls.small}" alt="${searchedForText}">
+        <figcaption>${searchedForText} by ${firstImg.user.name}</figcaption>
+      </figure>`;
+    } else {
+      htmlContent = '<div class="error-no-image">No images available</div>';
+    }
+     responseContainer.insertAdjacentHTML('afterbegin', htmlContent);   
+  };
+
 })();
